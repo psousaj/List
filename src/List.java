fc95dcf6e43d55dc82e3626f097045513a2d9f73
José <josepsousa2012@gmail.com>
2022-12-15 15:01:50 -0800

    First Commit

---
+++ src/List.java
@@ -0,0 +1,15 @@
+
+//CRUD: Create, Read, Update, Delete
+public interface List {
+	public void add(int value);
+	
+	public void remove(int value);
+	
+	public String show();
+	
+	public void update(int position, int value);
+	
+	public int size();
+
+	public boolean search(int value);
+}
